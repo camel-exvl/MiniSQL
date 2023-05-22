d3519bf6a56c9344746b75d371c93e43ed682a45
章越 <camel_zy@qq.com>
2023-05-22 11:11:07 +0800

    feat(bitmap_page): 实现位图页

--- src/page/bitmap_page.cpp
+++ src/page/bitmap_page.cpp
@@ -2,33 +2,49 @@
 
 #include "glog/logging.h"
 
-/**
- * TODO: Student Implement
- */
 template <size_t PageSize>
 bool BitmapPage<PageSize>::AllocatePage(uint32_t &page_offset) {
-  return false;
+    if (next_free_page_ == GetMaxSupportedSize()) {
+        return false;
+    }
+    bytes[next_free_page_ / 8] |= (1 << (next_free_page_ % 8));
+    page_offset = next_free_page_;
+    next_free_page_++;
+    while (next_free_page_ < GetMaxSupportedSize() && !IsPageFree(next_free_page_)) {
+        next_free_page_++;
+    }
+    return true;
 }
 
-/**
- * TODO: Student Implement
- */
 template <size_t PageSize>
 bool BitmapPage<PageSize>::DeAllocatePage(uint32_t page_offset) {
-  return false;
+    if (page_offset >= GetMaxSupportedSize()) {
+        return false;
+    }
+    if (IsPageFree(page_offset)) {
+        return false;
+    }
+    bytes[page_offset / 8] &= ~(1 << (page_offset % 8));
+    if (page_offset < next_free_page_) {
+        next_free_page_ = page_offset;
+    }
+    return true;
 }
 
-/**
- * TODO: Student Implement
- */
 template <size_t PageSize>
 bool BitmapPage<PageSize>::IsPageFree(uint32_t page_offset) const {
-  return false;
+    if (page_offset >= GetMaxSupportedSize()) {
+        return false;
+    }
+    return IsPageFreeLow(page_offset / 8, page_offset % 8);
 }
 
 template <size_t PageSize>
 bool BitmapPage<PageSize>::IsPageFreeLow(uint32_t byte_index, uint8_t bit_index) const {
-  return false;
+    if (byte_index >= MAX_CHARS || bit_index >= 8) {
+        return false;
+    }
+    return !(bytes[byte_index] & (1 << bit_index)); // 0 is free, 1 is allocated
 }
 
 template class BitmapPage<64>;
