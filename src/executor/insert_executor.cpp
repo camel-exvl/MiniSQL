29596c94eb21b8afd8fd6ae204ed7d3096fe4b4c
章越 <camel_zy@qq.com>
2023-06-23 19:25:02 +0800

    feat: 实现基本插入功能
    插入暂时没有判断unique限制，且未更新索引信息，之后再加

--- src/executor/insert_executor.cpp
+++ src/executor/insert_executor.cpp
@@ -9,8 +9,19 @@ InsertExecutor::InsertExecutor(ExecuteContext *exec_ctx, const InsertPlanNode *p
     : AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)) {}
 
 void InsertExecutor::Init() {
+  child_executor_->Init();
+  TableInfo *table_info;
+  exec_ctx_->GetCatalog()->GetTable(plan_->GetTableName(), table_info);
+  table_heap_ = table_info->GetTableHeap();
 }
 
 bool InsertExecutor::Next([[maybe_unused]] Row *row, RowId *rid) {
-  return false;
+  Row child_row{};
+  RowId child_rid{};
+  if (child_executor_->Next(&child_row, &child_rid)) {
+    // TODO: check constraint
+    return table_heap_->InsertTuple(child_row, exec_ctx_->GetTransaction());
+  } else {
+    return false;
+  }
 }
--- src/include/executor/executors/insert_executor.h
+++ src/include/executor/executors/insert_executor.h
@@ -45,6 +45,7 @@ class InsertExecutor : public AbstractExecutor {
   /** The insert plan node to be executed*/
   const InsertPlanNode *plan_;
   std::unique_ptr<AbstractExecutor> child_executor_;
+  TableHeap *table_heap_;
 };
 
 #endif  // MINISQL_INSERT_EXECUTOR_H
